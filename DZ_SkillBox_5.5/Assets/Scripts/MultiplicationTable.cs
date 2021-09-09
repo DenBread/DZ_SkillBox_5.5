34c0145d1cdfe7001c453df77d4bb6a0b78182e4
DenBread <37297335+DenBread@users.noreply.github.com>
2021-09-09 19:26:35 +0300

    test

--- DZ_SkillBox_5.5/Assets/Scripts/MultiplicationTable.cs
+++ DZ_SkillBox_5.5/Assets/Scripts/MultiplicationTable.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MultiplicationTable : MonoBehaviour
 {
+    [SerializeField] private Text[] textUI;
+
     private void Start()
     {
         Multiplication_table();
@@ -17,6 +20,7 @@ public class MultiplicationTable : MonoBehaviour
             for (int j = 1; j <= 10; j++)
             {
                 Debug.Log(i * j);
+                textUI[j].text = (i * j).ToString();
             }
         }
     }
